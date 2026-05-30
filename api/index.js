65655cff26ee9a511c88e53ec15f5aef2f88f249
Lakhan Laddha <lakhanladdha48@gmail.com>
2026-05-30 09:20:49 +0530

    Configure root dist folder build and add API serverless error boundary logger

--- api/index.js
+++ api/index.js
@@ -1,2 +1,30 @@
-const app = require('../server/server.js');
-module.exports = app;
+let app;
+let loadError = null;
+
+try {
+    app = require('../server/server.js');
+} catch (err) {
+    loadError = err;
+}
+
+module.exports = (req, res) => {
+    if (loadError) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(500).json({
+            error: "Express App Load Error",
+            message: loadError.message,
+            stack: loadError.stack
+        });
+    }
+    
+    try {
+        return app(req, res);
+    } catch (runError) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(500).json({
+            error: "Express App Execution Error",
+            message: runError.message,
+            stack: runError.stack
+        });
+    }
+};
